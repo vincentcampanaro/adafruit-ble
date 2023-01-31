c31eab94608c1a0d87aef48c2ca192b919fc2f43
vincentcampanaro <124087048+vincentcampanaro@users.noreply.github.com>
2023-01-31 14:37:28 -0500

    Add files via upload

---
+++ adafruit ble.py
@@ -0,0 +1,80 @@
+from adafruit_ble import BLERadio
+from adafruit_ble.advertising.standard import ProvideServicesAdvertisement
+from adafruit_ble.services.nordic import UARTService
+import board
+import digitalio
+import neopixel
+
+from adafruit_bluefruit_connect.packet import Packet
+from adafruit_bluefruit_connect.button_packet import ButtonPacket
+from adafruit_bluefruit_connect.color_packet import ColorPacket
+
+ble = BLERadio()
+ble.name = "VINCENT"
+uart = UARTService()
+uart_service = UARTService()
+advertisement = ProvideServicesAdvertisement(uart)
+led = digitalio.DigitalInOut(board.RED_LED)
+led.direction = digitalio.Direction.OUTPUT
+pixel = neopixel.NeoPixel(board.NEOPIXEL,1,brightness=.25)
+pixel2 = neopixel.NeoPixel(board.D5,300,brightness=.25)
+current_color = (0,0,0)
+pixel.fill(current_color)
+
+while True:
+    ble.start_advertising(advertisement)
+    while not ble.connected:
+        pass
+
+    # Now we're connected
+
+    while ble.connected:
+        if uart.in_waiting:
+            packet = Packet.from_stream(uart)
+            if isinstance(packet, ButtonPacket):
+                if packet.pressed:
+                    if packet.button == ButtonPacket.BUTTON_1:
+                        # The 1 button was pressed.
+                        pixel[0] = (255,0,0)
+                        print("LED RED")
+                    elif packet.button == ButtonPacket.BUTTON_2:
+                        # The 2 button was pressed.
+                        pixel[0] = (0,255,0)
+                        print("LED GREEN")
+                    elif packet.button == ButtonPacket.BUTTON_3:
+                        # The 3 button was pressed.
+                        pixel[0] = (0,0,255)
+                        print("LED BLUE")
+                    elif packet.button == ButtonPacket.BUTTON_4:
+                        # The 4 button was pressed.
+                        pixel[0] = (255,255,255)
+                        print("LED WHITE")
+                    elif packet.button == ButtonPacket.UP:
+                        # The UP button was pressed.
+                        sum = 0
+                        for i in range(50):
+                            sum = i
+                            if (sum % 2) == 0:
+                                pixel2[sum] = (0,255,0)
+                            if (sum % 2) == 1:
+                                pixel2[sum] = (255,0,0)
+
+                    elif packet.button == ButtonPacket.DOWN:
+                        # The DOWN button was pressed.
+                        led.value = False
+                        print("LED OFF")
+                    elif packet.button == ButtonPacket.LEFT:
+                        # The LEFT button was pressed.
+                        print("LEFT button pressed!")
+                    elif packet.button == ButtonPacket.RIGHT:
+                        # The RIGHT button was pressed.
+                        print("RIGHT button pressed!")
+            if isinstance(packet, ColorPacket):
+                print(packet.color)
+                pixel.fill(packet.color)
+                pixel2.fill(packet.color)
+                pixel2[1] = (0,255,0)
+
+# If we got here, we lost the connection. Go up to the top and start
+# advertising again and waiting for a connection.
+# Write your code here :-)
